b90d7cff96369541d40eac40b3d6485548c5bff2
Ayush Saini <ayush.saini.21cse@bmu.edu.in>
2023-05-21 13:36:29 +0530

    Express routes

--- server.js
+++ server.js
@@ -9,6 +9,13 @@ connectDB();
 
 app.get("/", (req, res) => res.send("API running"));
 
+//define routes
+
+app.use("/api/users", require("./routes/api/user"));
+app.use("/api/auth", require("./routes/api/auth"));
+app.use("/api/profile", require("./routes/api/profile"));
+app.use("/api/post", require("./routes/api/post"));
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
