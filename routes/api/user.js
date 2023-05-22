ce5fa7640a055aebed8e1f951e982225dcb254ca
Ayush Saini <ayush.saini.21cse@bmu.edu.in>
2023-05-22 15:23:28 +0530

    Mongo user data

--- routes/api/user.js
+++ routes/api/user.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const router = express.Router();
+const gravatar = require("gravatar");
+const bcrypt = require("bcryptjs");
 const { check, validationResult } = require("express-validator");
-
+const User = require("../../models/User");
 //@route    GET api/user
 //@desc     register User
 //@access   Public
@@ -15,13 +17,52 @@ router.post(
       "Please Enter a password with 6 or more characters"
     ).isLength({ min: 6 }),
   ],
-  (req, res) => {
+  async (req, res) => {
     // console.log(req.body);
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
-    res.send("User router");
+
+    const { name, email, password } = req.body;
+
+    try {
+      let user = await User.findOne({ email });
+
+      // See if the user Exist
+      if (user) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: "User already Exists" }] });
+      }
+
+      // Get user gravatar
+      const avatar = gravatar.url(email, {
+        s: "200",
+        r: "pg",
+        d: "mm",
+      });
+
+      user = new User({
+        name,
+        email,
+        avatar,
+        password,
+      });
+
+      // Encrpt the password
+      const salt = await bcrypt.genSalt(10);
+      user.password = await bcrypt.hash(password, salt);
+
+      await user.save();
+
+      // Return Json Webtoken
+
+      res.send("User Register");
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Server Error");
+    }
   }
 );
 
