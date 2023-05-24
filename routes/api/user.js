bb7385d9a3b2a1cbefe9aec940e360687c353fd9
Ayush Saini <ayush.saini.21cse@bmu.edu.in>
2023-05-24 22:08:36 +0530

    JWT token

--- routes/api/user.js
+++ routes/api/user.js
@@ -4,6 +4,8 @@ const gravatar = require("gravatar");
 const bcrypt = require("bcryptjs");
 const { check, validationResult } = require("express-validator");
 const User = require("../../models/User");
+const jwt = require("jsonwebtoken");
+const config = require("config");
 //@route    GET api/user
 //@desc     register User
 //@access   Public
@@ -57,8 +59,21 @@ router.post(
       await user.save();
 
       // Return Json Webtoken
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
 
-      res.send("User Register");
+      jwt.sign(
+        payload,
+        config.get("jwtSecret"),
+        { expiresIn: 360000 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
     } catch (err) {
       console.error(err.message);
       res.status(500).send("Server Error");
