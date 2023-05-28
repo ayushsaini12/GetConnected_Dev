54d5e1b93055528ef4ebca92208743e79a04ad79
Ayush Saini <ayush.saini.21cse@bmu.edu.in>
2023-05-28 17:08:24 +0530

    react initals

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import React, { Fragment } from "react";
+import Navbar from "./components/layout/Navbar";
+import Landing from "./components/layout/Landing";
+
+function App() {
+  return (
+    <Fragment>
+      <Navbar />
+      <Landing />
+    </Fragment>
+  );
+}
+
+export default App;
