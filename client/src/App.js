8f6bed64ea9c87b7607fd99c53d03f8e21176cba
Ayush Saini <ayush.saini.21cse@bmu.edu.in>
2023-05-28 21:58:28 +0530

    basic routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,26 @@
-import "./App.css";
 import React, { Fragment } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
 import Landing from "./components/layout/Landing";
+import "./App.css";
+import Login from "./components/auth/Login";
+import Register from "./components/auth/Register";
 
-function App() {
-  return (
+const App = () => (
+  <BrowserRouter>
     <Fragment>
       <Navbar />
-      <Landing />
+      <Routes>
+        <Route exact path="/" element={<Landing />} />
+      </Routes>
+      <section className="container">
+        <Routes>
+          <Route exact path="/register" element={<Register />} />
+          <Route exact path="/login" element={<Login />} />
+        </Routes>
+      </section>
     </Fragment>
-  );
-}
+  </BrowserRouter>
+);
 
 export default App;
