9868b8841fdb2554dd9ef962b34b60d9bb4755b2
Ayush Saini <ayush.saini.21cse@bmu.edu.in>
2023-06-05 16:09:49 +0530

    Private route dashboard

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,8 @@ import Landing from "./components/layout/Landing";
 import "./App.css";
 import Login from "./components/auth/Login";
 import Register from "./components/auth/Register";
+import Dashboard from "./components/dashboard/Dashboard";
+import PrivateRoute from "./components/routing/PrivateRoute";
 // Redux
 import { Provider } from "react-redux";
 import store from "./store";
@@ -20,21 +22,42 @@ const App = () => {
   useEffect(() => {
     store.dispatch(loadUser());
   }, []);
+
   return (
     <Provider store={store}>
       <BrowserRouter>
         <Fragment>
           <Navbar />
+          <Alert />
           <Routes>
-            <Route exact path="/" element={<Landing />} />
+            <Route path="/" element={<Landing />} />
+            <Route
+              path="/register"
+              element={
+                <section className="container">
+                  <Register />
+                </section>
+              }
+            />
+            <Route
+              path="/login"
+              element={
+                <section className="container">
+                  <Login />
+                </section>
+              }
+            />
+            <Route
+              path="/dashboard"
+              element={
+                <PrivateRoute>
+                  <section className="container">
+                    <Dashboard />
+                  </section>
+                </PrivateRoute>
+              }
+            />
           </Routes>
-          <section className="container">
-            <Alert />
-            <Routes>
-              <Route exact path="/register" element={<Register />} />
-              <Route exact path="/login" element={<Login />} />
-            </Routes>
-          </section>
         </Fragment>
       </BrowserRouter>
     </Provider>
